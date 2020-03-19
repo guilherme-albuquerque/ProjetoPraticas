ac3754ef5b32671c9abac4a70230b2e298b8bd28
Guilherme Albuquerque Rodrigues <61337784+guilherme-albuquerque@users.noreply.github.com>
2020-03-19 12:35:45 -0300

    Add files via upload

---
+++ src/main/java/dao/ConnectionFactory.java
@@ -0,0 +1,23 @@
+package dao;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+    static {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    // Obtém conexão com o banco de dados
+    public static Connection obtemConexao() throws SQLException {
+        return DriverManager
+                .getConnection("jdbc:mysql://localhost/phpmyadmin/sql.php?server=1&db=trabalho&table=pais&pos=0/pais?user=root&password=99449107Gui");
+    }
+
+}
